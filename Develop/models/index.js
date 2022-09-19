bc111d8428960702f1d22836e617934302b7f7fd
priyaaaryan <106102991+priyaaaryan@users.noreply.github.com>
2022-09-18 21:14:36 -0400

    added schema and built models

---
+++ Develop/models/index.js
@@ -0,0 +1,38 @@
+// import models
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
+const seedProducts = require("../seeds/product-seeds");
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  as: "category",
+  foreignKey: "category_id",
+  onDelete: "SET NULL",
+});
+
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+});
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  foreignKey: "product_id",
+  onDelete: "SET NULL",
+  through: ProductTag,
+});
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  foreignKey: "tag_id",
+  onDelete: "SET NULL",
+  through: ProductTag,
+});
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
